f578e4aa372427c6fd0f8e927ce6ada5df2d25c5
AKKS1013 <46385422+AKKS1013@users.noreply.github.com>
2024-08-25 15:57:08 +1000

    Forgot to push these mini-scraps (2)

--- main.js
+++ main.js
@@ -8,23 +8,27 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+const geometry = new THREE.TorusKnotGeometry(10, 2, 100, 20, 5, 4);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 
-const edges = new THREE.EdgesGeometry(geometry);
 const material_line = new THREE.LineBasicMaterial({ color: 0x0000ff });
-
-const line = new THREE.LineSegments(edges, material_line);
 const cube = new THREE.Mesh(geometry, material);
+
+const edges = new THREE.EdgesGeometry(geometry);
+const line = new THREE.LineSegments(edges, material_line);
+
 scene.add(cube);
 scene.add(line);
 
-camera.position.z = 5;
+camera.position.z = 25;
 
 function animate() {
-  cube.rotation.x += 0.02;
-  cube.rotation.y += 0.02;
-  line.rotation.x += 0.02;
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  line.rotation.x += 0.01;
+  line.rotation.y += 0.01;
 
   renderer.render(scene, camera);
+
 }
